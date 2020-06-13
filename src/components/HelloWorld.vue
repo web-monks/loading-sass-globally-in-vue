e98e0b8779a3b965413a766d8e5b67c11f21bd48
web-monks <webmonksmail@gmail.com>
2020-06-13 23:04:05 +0200

    use $yellow variable inside of HelloWorld component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,7 +40,10 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+h1 {
+  color: $yellow;
+}
 h3 {
   margin: 40px 0 0;
 }
